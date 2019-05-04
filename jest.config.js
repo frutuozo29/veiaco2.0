11be0f539074f0f3b11459cc546acd63868f2856
Renan Frutuozo <frutuozo29@gmail.com>
2019-05-03 22:48:57 -0300

    fix test

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,18 @@
 module.exports = {
   testEnvironment: "node",
-  reporters: ["default"]
+  reporters: ["default"],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  },
+  coveragePathIgnorePatterns: [
+    "<rootDir>/node_modules/",
+    "<rootDir>/coverage/",
+    "<rootDir>/database/",
+    "<rootDir>/routes/"
+  ]
 };
