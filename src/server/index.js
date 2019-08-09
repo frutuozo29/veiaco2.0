dbd995ec36c7b897fcaacdc40059629a56dad60d
Renan Frutuozo <frutuozo29@gmail.com>
2019-08-08 22:19:30 -0300

    Ativando playground em produção

--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,11 @@ const { ApolloServer } = require('apollo-server')
 const typeDefs = require('../graphql/schemas')
 const resolvers = require('../graphql/resolvers')
 
-const server = new ApolloServer({ typeDefs, resolvers })
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: true,
+  playground: true
+})
 
 module.exports = server
